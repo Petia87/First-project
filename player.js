26daedfc8f0194f719b386b20518f95c73c83f13
Petq <alex5577@abv.bg>
2021-04-16 18:13:31 +0300

    fin

--- player.js
+++ player.js
@@ -1,2 +1,33 @@
+let buttonPlay=document.getElementById('player').play()
+let buttonPause=document.getElementById('player').pause()
+let buttonValue1=document.getElementById('player').volume+=0.1
+let buttonValue2=document.getElementById('player').volume-=0.1
 
-console.log(1);
+
+/*if (command==buttonPlay) {
+    buttonPlay.style.display='inline'
+    buttonPause.style.display='none'
+} else if (command==buttonPause) {
+    buttonPlay.style.display='none'
+    buttonPause.style.display='inline'
+}
+let command=0
+if (command==buttonPlay) {
+    buttonPlay.style.display='inline'
+    
+} else {
+    buttonPlay.style.display='none'
+ 
+}
+function playPause() {
+   let  mediaPlayer = document.getElementById('player');
+    if (mediaPlayer.paused) {
+        mediaPlayer.play(); 
+        buttonPlay.style.display='inline'
+        buttonPause.style.display='none'
+    } else {
+        mediaPlayer.pause(); 
+        buttonPlay.style.display='none'
+        buttonPause.style.display='inline'
+    }
+}

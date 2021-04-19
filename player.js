b80df586b79d0d43b3d7818da5dc0c7526a2c83e
Petq <alex5577@abv.bg>
2021-04-19 13:59:02 +0300

    k

--- player.js
+++ player.js
@@ -2,20 +2,20 @@ const play = document.querySelector('.play')
 const pause = document.querySelector('.pause')
 const video = document.querySelector('video')
 const progress = document.querySelector('.progress')
-const volume= document.querySelector('.volume')
+const volume = document.querySelector('.volume')
 
-play.addEventListener('click',function() {
+play.addEventListener('click', function () {
     video.play()
 })
-pause.addEventListener('click',function() {
+pause.addEventListener('click', function () {
     video.pause()
 })
 
-video.addEventListener('timeupdate',function() {
-    progress.value=video.currentTime/video.duration
+video.addEventListener('timeupdate', function () {
+    progress.value = video.currentTime / video.duration
 })
-volume.addEventListener('change',function (e) {
-    video.volume=e.currentTarget.value/100
+volume.addEventListener('change', function (e) {
+    video.volume = e.currentTarget.value / 100
 })
 
 /*const vid eoPlayer = document.querySelector(".video-player")

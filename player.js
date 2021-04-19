815c5e21d95ce78b7529f58d1b4ff84f70c4b38d
Petq <alex5577@abv.bg>
2021-04-19 13:55:11 +0300

    final

--- player.js
+++ player.js
@@ -1,59 +1,28 @@
-let x = document.getElementById("vplayer"); 
+const play = document.querySelector('.play')
+const pause = document.querySelector('.pause')
+const video = document.querySelector('video')
+const progress = document.querySelector('.progress')
+const volume= document.querySelector('.volume')
+
+play.addEventListener('click',function() {
+    video.play()
+})
+pause.addEventListener('click',function() {
+    video.pause()
+})
+
+video.addEventListener('timeupdate',function() {
+    progress.value=video.currentTime/video.duration
+})
+volume.addEventListener('change',function (e) {
+    video.volume=e.currentTarget.value/100
+})
+
+/*const vid eoPlayer = document.querySelector(".video-player")
+const video = videoPlayer.querySelector(".video")
+const playerButton = videoPlayer.querySelector(".play-button")
 
 
-function playVideo() { 
-    x.play(); 
-} 
+//Play and Pause button
 
-function pauseVideo() { 
-    x.pause(); 
-} 
-
-
-function prevVideo() { 
-    x.pause(); 
-} 
-
-
-function nextVideo() { 
-    x.pause(); 
-} 
-
-
-
-/*
-let buttonPlay=document.getElementById('player').play()
-let buttonPause=document.getElementById('player').pause()
-let buttonValue1=document.getElementById('player').volume+=0.1
-let buttonValue2=document.getElementById('player').volume-=0.1
-
-
-if (command==buttonPlay) {
-    buttonPlay.style.display='inline'
-    buttonPause.style.display='none'
-} else if (command==buttonPause) {
-    buttonPlay.style.display='none'
-    buttonPause.style.display='inline'
-}
-let command=0
-if (command==buttonPlay) {
-    buttonPlay.style.display='inline'
-    
-} else {
-    buttonPlay.style.display='none'
- 
-}
-function playPause() {
-   let  mediaPlayer = document.getElementById('player');
-    if (mediaPlayer.paused) {
-        mediaPlayer.play(); 
-        buttonPlay.style.display='inline'
-        buttonPause.style.display='none'
-    } else {
-        mediaPlayer.pause(); 
-        buttonPlay.style.display='none'
-        buttonPause.style.display='inline'
-    }
-}
 */
-console.log(1);

c0f069f79c29093cf2acc37f49511c2843559015
Petia Valchkova <peps@gmail.com>
2022-02-25 17:14:04 +0200

    new button

--- player.js
+++ player.js
@@ -3,6 +3,8 @@ const pause = document.querySelector('.pause')
 const video = document.querySelector('video')
 const progress = document.querySelector('.progress')
 const volume = document.querySelector('.volume')
+//const videoElement = document.getElementById('video');
+const pipButton = document.getElementById('pipButton');
 
 play.addEventListener('click', function () {
     video.play()
@@ -18,11 +20,57 @@ volume.addEventListener('change', function (e) {
     video.volume = e.currentTarget.value / 100
 })
 
-/*const vid eoPlayer = document.querySelector(".video-player")
-const video = videoPlayer.querySelector(".video")
-const playerButton = videoPlayer.querySelector(".play-button")
 
+if ('pictureInPictureEnabled' in document) {
+    pipButton.classList.remove('hidden')
+    pipButton.disabled = false;
+    
+    pipButton.addEventListener('click', () => {
+      if (document.pictureInPictureElement) {
+        document
+          .exitPictureInPicture()
+          .catch(error => {
+            // Error handling
+          })
+      } else {
+        video
+        .requestPictureInPicture()
+        .catch(error => {
+          // Error handling
+        });
+      }
+    });
+  }
+  
+  video.addEventListener('enterpictureinpicture', () => {
+      pipButton.textContent = '■';
+  });
+  
+  video.addEventListener('leavepictureinpicture', () => {
+      pipButton.textContent = '□';
+  });
+/*
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error Here
+  }
+}
 
-//Play and Pause button
+button.addEventListener('click', async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
 
-*/
+// On Load
+selectMediaStream();*/
